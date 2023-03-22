77f61d24b810cdfe872caf78cb9690e86fc158d3
Shigeru Hagiwara <hgwrsgr@gmail.com>
2023-03-22 20:42:32 +0900

    cache

--- src/main/java/jp/moreslowly/oi/config/WebMvcConfig.java
+++ src/main/java/jp/moreslowly/oi/config/WebMvcConfig.java
@@ -1,7 +1,11 @@
 package jp.moreslowly.oi.config;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,5 +19,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
             "http://127.0.0.1:5173",
             "http://localhost:5173",
             "http://192.168.1.175:5173");
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/index.html")
+        .addResourceLocations("classpath:/static/")
+        .setCacheControl(CacheControl.maxAge(0, TimeUnit.SECONDS).cachePrivate());
   }
 }
