1bd9d465c14f166069527ee946b954b141c88cd3
Shigeru Hagiwara <hgwrsgr@gmail.com>
2023-04-27 21:47:59 +0900

    room size 200

--- src/main/java/jp/moreslowly/oi/common/RoomLimitation.java
+++ src/main/java/jp/moreslowly/oi/common/RoomLimitation.java
@@ -1,7 +1,7 @@
 package jp.moreslowly.oi.common;
 
 public final class RoomLimitation {
-  public static final int MAX_ROOM_SIZE = 20;
+  public static final int MAX_ROOM_SIZE = 200;
   public static final int MAX_MEMBER_SIZE = 6;
   public static final int MAX_HAND_OUT_SIZE = 7;
 
